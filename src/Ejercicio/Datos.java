6974210c6e694381722fe9b259c39b99e8e965fa
Delix <gonzalodex@hotmail.com>
2023-03-27 16:54:34 -0300

    Se agrega el proyecto

---
+++ src/Ejercicio/Datos.java
@@ -0,0 +1,19 @@
+package Ejercicio;
+
+public class Datos {
+
+    public static void main(String[] args) {
+
+        int valorInt = 14;
+        long valorLong = 21390123;
+        double valorDouble = 2.439430;
+        boolean valorBoolean = true;
+        String cadena = "Hello World";
+
+        System.out.println("Int: " + valorInt);
+        System.out.println("Long: " + valorLong);
+        System.out.println("Double: " + valorDouble);
+        System.out.println("Boolean: " + valorBoolean);
+        System.out.println("String: " + cadena);
+    }
+}
